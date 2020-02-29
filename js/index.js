36b8ef1aedbd6856ee0850088e7ec18cb7fa50f3
kenny_svg <fagbenrokenny44@gmail.com>
2020-02-29 03:30:16 +0100

    i made changes on the frontend

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function Display() {
+    var x = document.getElementById('demo'); 
+        x.style.backgroundColor = "whitesmoke";
+        x.style.display = "block";
+        x.style.borderRadius = "10px";
+}           
+function Cancel() {
+        var x = document.getElementById('demo');
+        x.style.display = "none";
+}
+function Dbclick() {
+        var x = document.getElementById('demo');
+        x.style.display = "none";
+}
